0bb1c67f89592aeed2324fd6f6843a629410e278
Stefan Cvetanovski <stefan.cvetanovski@polarcape.com>
2023-04-02 22:47:44 +0200

    Make keyboard qwerty

--- src/components/ui/LettersList/LettersList.tsx
+++ src/components/ui/LettersList/LettersList.tsx
@@ -33,9 +33,18 @@ const LettersList: React.FC<LettersListProps> = ({
   };
 
   return (
-    <div className="bg-gray-100 rounded-md p-4">
-      <div className="grid grid-cols-10 gap-2">
-        {letters.map((letter) => (
+    <div className="bg-gray-100 rounded-md p-3">
+      <div className="grid grid-cols-11 gap-2">
+        {letters.slice(0, 11).map((letter) => (
+          <div
+            key={letter}
+            className={`rounded-md flex items-center justify-center text-gray-800 font-bold text-2xl cursor-pointer shadow-md hover:shadow-lg transition duration-200 p-4`}
+            onClick={() => handleLetterClick(letter)}
+          >
+            {letter}
+          </div>
+        ))}
+        {letters.slice(11, 22).map((letter) => (
           <div
             key={letter}
             className={`rounded-md flex items-center justify-center text-gray-800 font-bold text-2xl cursor-pointer shadow-md hover:shadow-lg transition duration-200 p-4`}
@@ -45,11 +54,20 @@ const LettersList: React.FC<LettersListProps> = ({
           </div>
         ))}
         <div
-          className="bg-red-600  col-span-2 rounded-md flex items-center justify-center text-white font-bold text-1xl cursor-pointer shadow-md hover:shadow-lg transition duration-200"
+          className="bg-red-600  col-span-1 rounded-md flex items-center justify-center text-white font-bold text-1xl cursor-pointer shadow-md hover:shadow-lg transition duration-200"
           onClick={handleDeleteClick}
         >
           &#x232b;
         </div>
+        {letters.slice(22, -1).map((letter) => (
+          <div
+            key={letter}
+            className={`rounded-md flex items-center justify-center text-gray-800 font-bold text-2xl cursor-pointer shadow-md hover:shadow-lg transition duration-200 p-4`}
+            onClick={() => handleLetterClick(letter)}
+          >
+            {letter}
+          </div>
+        ))}
         <div
           onClick={() => onEnter()}
           className="bg-gray-300 col-span-2 rounded-md flex items-center justify-center text-gray-800 font-bold text-2xl cursor-pointer shadow-md hover:shadow-lg transition duration-200"
